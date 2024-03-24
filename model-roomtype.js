2af94a3bb7d019a764201677e437d7957ba540d5
meriex <145168915+meriex@users.noreply.github.com>
2024-03-24 05:10:22 +0100

    Create model-roomtype.js

---
+++ model-roomtype.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const roomTypeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const RoomType = mongoose.model('RoomType', roomTypeSchema);
+
+module.exports = RoomType;
