2e8457c28924acc94a3a14de05fd70df95991cf6
meriex <145168915+meriex@users.noreply.github.com>
2024-03-24 22:16:51 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  roomType: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'RoomType',
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = Room;
